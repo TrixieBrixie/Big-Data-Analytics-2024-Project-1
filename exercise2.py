6a7e52fb9670e3d0a7e35729ee5bdaba3890c970
Michele <michele.ru80@gmail.com>
2024-06-07 19:40:33 +0100

    Script Updated

--- exercise2.py
+++ exercise2.py
@@ -1,154 +1,154 @@
-### Exercise 2: Downloading and processing text files
-'''
-Using parallel programming techniques, develop a Python script to download books from the provided URLs. 
-The script should efficiently handle the download process in parallel to optimise performance.
-'''
+# ### Exercise 2: Downloading and processing text files
+# '''
+# Using parallel programming techniques, develop a Python script to download books from the provided URLs. 
+# The script should efficiently handle the download process in parallel to optimise performance.
+# '''
 
-urls = [
-        'http://www.gutenberg.org/files/1342/1342-0.txt',  # Pride and Prejudice by Jane Austen
-        'http://www.gutenberg.org/cache/epub/11/pg11.txt',  # Alice’s Adventures in Wonderland by Lewis Carroll
-        'http://www.gutenberg.org/files/84/84-0.txt',  # Frankenstein by Mary Shelley
-        'http://www.gutenberg.org/files/1080/1080-0.txt',  # A Modest Proposal by Jonathan Swift
-        'http://www.gutenberg.org/files/98/98-0.txt',  # A Tale of Two Cities by Charles Dickens
-        'http://www.gutenberg.org/files/2701/2701-0.txt',  # Moby Dick by Herman Melville
-        'http://www.gutenberg.org/files/2600/2600-0.txt',  # War and Peace by Leo Tolstoy
-        'http://www.gutenberg.org/files/174/174-0.txt',  # The Picture of Dorian Gray by Oscar Wilde
-        'http://www.gutenberg.org/files/43/43-0.txt',  # The Strange Case of Dr. Jekyll and Mr. Hyde by Robert Louis Stevenson
-        'http://www.gutenberg.org/files/1661/1661-0.txt'  # The Adventures of Sherlock Holmes by Arthur Conan Doyle
-    ]
+# urls = [
+#         'http://www.gutenberg.org/files/1342/1342-0.txt',  # Pride and Prejudice by Jane Austen
+#         'http://www.gutenberg.org/cache/epub/11/pg11.txt',  # Alice’s Adventures in Wonderland by Lewis Carroll
+#         'http://www.gutenberg.org/files/84/84-0.txt',  # Frankenstein by Mary Shelley
+#         'http://www.gutenberg.org/files/1080/1080-0.txt',  # A Modest Proposal by Jonathan Swift
+#         'http://www.gutenberg.org/files/98/98-0.txt',  # A Tale of Two Cities by Charles Dickens
+#         'http://www.gutenberg.org/files/2701/2701-0.txt',  # Moby Dick by Herman Melville
+#         'http://www.gutenberg.org/files/2600/2600-0.txt',  # War and Peace by Leo Tolstoy
+#         'http://www.gutenberg.org/files/174/174-0.txt',  # The Picture of Dorian Gray by Oscar Wilde
+#         'http://www.gutenberg.org/files/43/43-0.txt',  # The Strange Case of Dr. Jekyll and Mr. Hyde by Robert Louis Stevenson
+#         'http://www.gutenberg.org/files/1661/1661-0.txt'  # The Adventures of Sherlock Holmes by Arthur Conan Doyle
+#     ]
 
-# Here are supporting scripts to help you start:
+# # Here are supporting scripts to help you start:
 
-# 1 - The following script demonstrates the download of a single book using the `requests` library.
+# # 1 - The following script demonstrates the download of a single book using the `requests` library.
 
-import requests
-url = 'http://www.gutenberg.org/cache/epub/11/pg11.txt'
-# URL of the text file to be downloaded
-response = requests.get(url)
-# Get the content of the response as a string
-book = response.text
-print(book[1452:1800])
-
-
-# 2 - The following script demonstrates how to extract the book name from the URL. 
-# For example, extracting `1661-0.txt` from `http://www.gutenberg.org/files/1661/1661-0.txt` . 
-import re
-
-# URL containing the filename
-url = "http://www.gutenberg.org/files/1661/1661-0.txt"
-
-# Regex to extract the filename, in this case 1661-0.txt
-pattern = r'[^/]+$'
-
-# Search the URL for the filename using the regex pattern
-filename = re.search(pattern, url).group(0) '''The group() method of a match object returns one or more subgroups of the match.
-group(0) returns the entire match. This is the same as calling group() with no arguments.
-group(1), group(2), etc., return the first, second, etc., capturing groups from the pattern.'''
-
-# It's a good idea to extract the book name so we can save it with the corresponding filename.
-print("Extracted filename:", filename, "for:", url)
+# import requests
+# url = 'http://www.gutenberg.org/cache/epub/11/pg11.txt'
+# # URL of the text file to be downloaded
+# response = requests.get(url)
+# # Get the content of the response as a string
+# book = response.text
+# print(book[1452:1800])
 
 
-# 3 - Finally, the following script demonstrates how to save a text list into a `txt` file.
+# # 2 - The following script demonstrates how to extract the book name from the URL. 
+# # For example, extracting `1661-0.txt` from `http://www.gutenberg.org/files/1661/1661-0.txt` . 
+# import re
 
-text_list = ['hello','world','!']
-filename = 'myfile.txt'
-with open(filename, 'w', encoding='utf-8') as file:
-  for text in text_list:
-    file.write(text + '\n')
+# # URL containing the filename
+# url = "http://www.gutenberg.org/files/1661/1661-0.txt"
+
+# # Regex to extract the filename, in this case 1661-0.txt
+# pattern = r'[^/]+$'
+
+# # Search the URL for the filename using the regex pattern
+# # filename = re.search(pattern, url).group(0) '''The group() method of a match object returns one or more subgroups of the match.
+# # group(0) returns the entire match. This is the same as calling group() with no arguments.
+# # group(1), group(2), etc., return the first, second, etc., capturing groups from the pattern.'''
+
+# # It's a good idea to extract the book name so we can save it with the corresponding filename.
+# print("Extracted filename:", filename, "for:", url)
 
 
-# Exercise 2
-import requests
-import re
-import time
-import multiprocessing
+# # 3 - Finally, the following script demonstrates how to save a text list into a `txt` file.
+
+# text_list = ['hello','world','!']
+# filename = 'myfile.txt'
+# with open(filename, 'w', encoding='utf-8') as file:
+#   for text in text_list:
+#     file.write(text + '\n')
 
 
-'''
-Considering the above code samples, create a function to store a list of strings into a file.
-Use the following specifications. 
-The function does not return a value, but it creates a file within your current folder.
-'''
+# # Exercise 2
+# import requests
+# import re
+# import time
+# import multiprocessing
 
-def save_text_to_file(text, filename):
-    filename = 'myfile.txt'
-    with open(filename, 'w', encoding='utf-8') as file:
-        file.write(text)
 
-'''
-Develop a function to download and save a book using a specific `url`.  
-The function should download the book using the url, and then use the `save_text_to_file` to export the file.
-Feel free to reuse the code samples above to export the file name from the `url`.
-'''
-def download_book(url):
-    response = requests.get(url)
-    book = response.text
-    pattern = r'[^/]+$'
-    match = re.search(pattern, url)
-    title = match.group(0)
-    save_text_to_file(book, title)
-    return book
+# '''
+# Considering the above code samples, create a function to store a list of strings into a file.
+# Use the following specifications. 
+# The function does not return a value, but it creates a file within your current folder.
+# '''
 
-# Serial vs multiprocessing**
+# def save_text_to_file(text, filename):
+#     filename = 'myfile.txt'
+#     with open(filename, 'w', encoding='utf-8') as file:
+#         file.write(text)
 
-'''
-Create a function named `serial_downloader()` that serially downloads books and measures the time it takes to complete the downloads. 
-You will need to use Python’s `requests` library to fetch the data from the internet and the `time` module to keep track of the duration.
-Then, use the `multiprocessing.Process` to download the books in parallel. Measure the time taken to complete the downloads.
-Compare the serial and parallel cases. Do you notice a difference?
-'''
+# '''
+# Develop a function to download and save a book using a specific `url`.  
+# The function should download the book using the url, and then use the `save_text_to_file` to export the file.
+# Feel free to reuse the code samples above to export the file name from the `url`.
+# '''
+# def download_book(url):
+#     response = requests.get(url)
+#     book = response.text
+#     pattern = r'[^/]+$'
+#     match = re.search(pattern, url)
+#     title = match.group(0)
+#     save_text_to_file(book, title)
+#     return book
 
-def serial_downloader(urls):
-    start = time.perf_counter()
-    for url in urls:
-        download_book(url)
-    end = time.perf_counter()
-    print(f'Serial run took {round(end-start,2)} second(s)')
+# # Serial vs multiprocessing**
 
-def parallel_downloader(urls):
-    start = time.perf_counter()
-    processes = []
-    for url in urls:
-        p = multiprocessing.Process(target=download_book, args=(url,))
-        p.start()
-        processes.append(p)
-    for p in processes:
-        p.join()
-    end = time.perf_counter()
-    print(f'Parallel run took {round(end-start,2)} second(s)')
+# '''
+# Create a function named `serial_downloader()` that serially downloads books and measures the time it takes to complete the downloads. 
+# You will need to use Python’s `requests` library to fetch the data from the internet and the `time` module to keep track of the duration.
+# Then, use the `multiprocessing.Process` to download the books in parallel. Measure the time taken to complete the downloads.
+# Compare the serial and parallel cases. Do you notice a difference?
+# '''
 
-def parallel_runner1():
-    start = time.perf_counter()
+# def serial_downloader(urls):
+#     start = time.perf_counter()
+#     for url in urls:
+#         download_book(url)
+#     end = time.perf_counter()
+#     print(f'Serial run took {round(end-start,2)} second(s)')
 
-    t1 = threading.Thread(target=foo)
-    t2 = threading.Thread(target=foo)
-    t3 = threading.Thread(target=foo)
+# def parallel_downloader(urls):
+#     start = time.perf_counter()
+#     processes = []
+#     for url in urls:
+#         p = multiprocessing.Process(target=download_book, args=(url,))
+#         p.start()
+#         processes.append(p)
+#     for p in processes:
+#         p.join()
+#     end = time.perf_counter()
+#     print(f'Parallel run took {round(end-start,2)} second(s)')
 
-    t1.start()
-    t2.start()
-    t3.start()
+# def parallel_runner1():
+#     start = time.perf_counter()
 
-    t1.join()
-    t2.join()
-    t3.join()
+#     t1 = threading.Thread(target=foo)
+#     t2 = threading.Thread(target=foo)
+#     t3 = threading.Thread(target=foo)
 
-    end = time.perf_counter()
-    print(f'Parallel: {end - start} second(s)')
+#     t1.start()
+#     t2.start()
+#     t3.start()
 
-if __name__ == '__main__':
-    urls = [
-        'http://www.gutenberg.org/files/1342/1342-0.txt',  # Pride and Prejudice by Jane Austen
-        'http://www.gutenberg.org/cache/epub/11/pg11.txt',  # Alice’s Adventures in Wonderland by Lewis Carroll
-        'http://www.gutenberg.org/files/84/84-0.txt',  # Frankenstein by Mary Shelley
-        'http://www.gutenberg.org/files/1080/1080-0.txt',  # A Modest Proposal by Jonathan Swift
-        'http://www.gutenberg.org/files/98/98-0.txt',  # A Tale of Two Cities by Charles Dickens
-        'http://www.gutenberg.org/files/2701/2701-0.txt',  # Moby Dick by Herman Melville
-        'http://www.gutenberg.org/files/2600/2600-0.txt',  # War and Peace by Leo Tolstoy
-        'http://www.gutenberg.org/files/174/174-0.txt',  # The Picture of Dorian Gray by Oscar Wilde
-        'http://www.gutenberg.org/files/43/43-0.txt',  # The Strange Case of Dr. Jekyll and Mr. Hyde by Robert Louis Stevenson
-        'http://www.gutenberg.org/files/1661/1661-0.txt'  # The Adventures of Sherlock Holmes by Arthur Conan Doyle
-    ]
-    print(f"Downloading and processing {len(urls)} file(s)...")
-    serial_downloader(urls)
-    parallel_downloader(urls)
+#     t1.join()
+#     t2.join()
+#     t3.join()
+
+#     end = time.perf_counter()
+#     print(f'Parallel: {end - start} second(s)')
+
+# if __name__ == '__main__':
+#     urls = [
+#         'http://www.gutenberg.org/files/1342/1342-0.txt',  # Pride and Prejudice by Jane Austen
+#         'http://www.gutenberg.org/cache/epub/11/pg11.txt',  # Alice’s Adventures in Wonderland by Lewis Carroll
+#         'http://www.gutenberg.org/files/84/84-0.txt',  # Frankenstein by Mary Shelley
+#         'http://www.gutenberg.org/files/1080/1080-0.txt',  # A Modest Proposal by Jonathan Swift
+#         'http://www.gutenberg.org/files/98/98-0.txt',  # A Tale of Two Cities by Charles Dickens
+#         'http://www.gutenberg.org/files/2701/2701-0.txt',  # Moby Dick by Herman Melville
+#         'http://www.gutenberg.org/files/2600/2600-0.txt',  # War and Peace by Leo Tolstoy
+#         'http://www.gutenberg.org/files/174/174-0.txt',  # The Picture of Dorian Gray by Oscar Wilde
+#         'http://www.gutenberg.org/files/43/43-0.txt',  # The Strange Case of Dr. Jekyll and Mr. Hyde by Robert Louis Stevenson
+#         'http://www.gutenberg.org/files/1661/1661-0.txt'  # The Adventures of Sherlock Holmes by Arthur Conan Doyle
+#     ]
+#     print(f"Downloading and processing {len(urls)} file(s)...")
+#     serial_downloader(urls)
+#     parallel_downloader(urls)
--- project1.py
+++ project1.py
@@ -1,14 +1,64 @@
 import re
 import time
-import requests
 import multiprocessing
 import threading
 import logging
+import os
+from pytube import YouTube
+from moviepy.editor import VideoFileClip
+from pathlib import Path
+import speech_recognition as sr
 
 # List to store the extracted video names
 videoenames = []
 
-# Extract the URLs from video_urls.txt.
+# Create a lock object to ensure only one thread writes to the file at a time
+file_lock = threading.Lock()
+
+# Ensure the directories exist before configuring the logger
+log_directory = 'video_records'
+os.makedirs(log_directory, exist_ok=True)
+
+# Directory to save downloaded videos
+download_directory = 'downloaded_videos'
+os.makedirs(download_directory, exist_ok=True)
+
+# Directory to save extracted audio files
+audio_directory = 'audios'
+os.makedirs(audio_directory, exist_ok=True)
+
+# Directory to save transcriptions
+text_directory = 'texts'
+os.makedirs(text_directory, exist_ok=True)
+
+# Configure logging
+logging.basicConfig(
+    filename=os.path.join(log_directory, 'download_log.txt'),
+    level=logging.INFO,
+    format='"Timestamp": %(asctime)s, "URL":"%(url)s", "Thread": "%(threadName)s"',
+    datefmt='%H:%M, %d %b %Y'
+)
+logger = logging.getLogger()
+
+def video_record(url, thread_name):
+    # Create path to the log file (download_log.txt) located within a directory specified by log_directory
+    file_path = os.path.join(log_directory, 'download_log.txt')
+    # Ensure threads don't write simultaneously
+    with file_lock:
+         # Open the file in append mode.
+        with open(file_path, "a") as file:
+            # Create a log entry with the generated phrase
+            log_entry = f'"Timestamp": {time.strftime("%H:%M, %d %b %Y")}, "URL":"{url}", "Thread":"{thread_name}"\n'
+             # Write the log entry to the file
+            file.write(log_entry)
+
+def download_video_and_log(url):
+    download_video(url)
+    # Record which video was downloaded by which process or thread
+    thread_name = threading.current_thread().name
+    video_record(url, thread_name)
+
+# Extract the URLs from video_urls.txt
 def extract_urls(youtube_videos):
     video_urls = []
     with open(youtube_videos, 'r', encoding='utf-8') as file:
@@ -22,34 +72,28 @@ def extract_urls(youtube_videos):
             videoenames.append(videoename)
     return video_urls
 
-# Function to download video content
-def download_video(url,):
+# Download video content
+def download_video(url):
     try:
-        response = requests.get(url)
-        # Store the raw binary content of the response body (the actual video data).
-        video = response.content
-        pattern = r'[^/]+$'
-        match = re.search(pattern, url)
-        # Store the name of the file
-        title = match.group(0)
-        filename = title 
-        with open(filename, 'wb') as file:
-            file.write(video)
-        print(f'Downloaded {filename}')
-    except requests.RequestException as e:
+        yt = YouTube(url)
+        stream = yt.streams.filter(progressive=True, file_extension='mp4').first()
+        filename = stream.download(output_path=download_directory)
+        print(f'{url} downloaded.')
+        extract_audio(filename, audio_directory)
+    except Exception as e:
         print(f'Error downloading {url}: {e}')
-    
-def serial_downloader(videonames):
+
+def serial_downloader(video_urls):
     start = time.perf_counter()
-    for video in videonames:
-        download_video(video)
+    for video in video_urls:
+        download_video_and_log(video)
     end = time.perf_counter()
     print(f'Serial run took {round(end-start, 2)} second(s)')
 
-def parallel_downloader(videonames):
+def parallel_downloader(video_urls):
     start = time.perf_counter()
     processes = []
-    for video in videonames:
+    for video in video_urls:
         p = multiprocessing.Process(target=download_video, args=(video,))
         p.start()
         processes.append(p)
@@ -58,92 +102,119 @@ def parallel_downloader(videonames):
     end = time.perf_counter()
     print(f'Parallel run took {round(end-start, 2)} second(s)')
 
-def parallel_downloader1(videonames):
+def parallel_downloader1(video_urls):
     start = time.perf_counter()
-
     threads = []
-    for video in videonames:
-        t = threading.Thread(target=download_video, args=(video,))
+    for video in video_urls:
+        t = threading.Thread(target=download_video_and_log, args=(video,))
         t.start()
         threads.append(t)
-
     for t in threads:
         t.join()
-
     end = time.perf_counter()
     print(f'Parallel run with threads took {round(end-start, 2)} second(s)')
 
 # Limit to 5 concurrent downloads
-semaphore = threading.Semaphore(5) 
+semaphore = threading.Semaphore(5)
 
-def parallel_downloader2(videonames):
+def parallel_downloader2(video_urls):
     start = time.perf_counter()
     
     def download_with_semaphore(video):
         with semaphore:
-            download_video(video)
+            download_video_and_log(video)
     
     threads = []
-    for video in videonames:
+    for video in video_urls:
         t = threading.Thread(target=download_with_semaphore, args=(video,))
         t.start()
         threads.append(t)
-
     for t in threads:
         t.join()
     
     end = time.perf_counter()
     print(f'Parallel run with limited threads took {round(end-start, 2)} second(s)')
 
-'''
-serial_downloder : time complexity is O(n). The function downloads each video sequentially, resulting in a linear time complexity.
-Space complexity is O(1). The function does not require additional space that grows with the number of videos.
+def extract_audio(video_path, audio_directory):
+    Path(audio_directory).mkdir(parents=True, exist_ok=True)
+    
+    try:
+        video = VideoFileClip(video_path)
+        video_path_obj = Path(video_path)
+        audio_filename = video_path_obj.stem + '.wav'
+        audio_path = Path(audio_directory) / audio_filename
+        video.audio.write_audiofile(str(audio_path))
+        print(f"Audio extracted and saved to {audio_path}")
+        return audio_path
+    except Exception as e:
+        print(f"Failed to extract audio from {video_path}: {e}")
+        return None
+    finally:
+        if video:
+            video.close()
 
-parallel_downloader's time complexity remains linear but the time can be significantly reduced due to concurrent downloads. The total time will depend on the number of available CPU cores.
-Space Complexity is O(n). Each process takes some amount of memory, so the space complexity is proportional to the number of processes created.
-
-parallel_downloader1's time complexity is O(n). The function downloads videos concurrently, but the overall time complexity remains linear.
-Space Complexity is O(n). Space complexity grows linearly with the number of videos.
-
-parallel_downloader2's time complexity is O(n). The function's time complexity is linear, similar to the previous parallel approaches.
-Space Complexity is O(n). Although the semaphore limits the number of active threads to 5, the function still creates a thread for each video, resulting in linear space complexity.
-
-'''
+def audio_to_text(audio_path, text_directory):
+    Path(text_directory).mkdir(parents=True, exist_ok=True)
+    recognizer = sr.Recognizer()
+    try:
+        with sr.AudioFile(str(audio_path)) as source:
+            audio = recognizer.record(source)
+        text = recognizer.recognize_google(audio)
+        print(f"Recognized text: {text}")
+    except sr.UnknownValueError:
+        print(f"Speech Recognition could not understand audio in {audio_path}")
+        text = ""
+    except sr.RequestError as e:
+        print(f"Request error for: {e}")
+        text = ""
+    except Exception as e:
+        print(f"Error processing audio file {audio_path}: {e}")
+        text = ""
+    
+    audio_path_obj = Path(audio_path)
+    text_filename = audio_path_obj.stem + '.txt'
+    text_path = Path(text_directory) / text_filename
+    
+    try:
+        with open(text_path, 'w', encoding='utf-8') as text_file:
+            text_file.write(text)
+        print(f"Text converted and saved to {text_path}")
+    except Exception as e:
+        print(f"Failed to save text file {text_path}: {e}")
 
 if __name__ == '__main__':
-    urls_YouTube = ['https://www.youtube.com/shorts/CS3kviWGkH0',
-                    'https://www.youtube.com/shorts/-WowH0liGfE',
-                    'https://www.youtube.com/shorts/QTyQtQmpZ0g',
-                    'https://www.youtube.com/shorts/KDqXneUN4CA',
-                    'https://www.youtube.com/shorts/AnOJRqLnfqQ',
-                    'https://www.youtube.com/shorts/LcQa94wX-O8',
-                    'https://www.youtube.com/shorts/z-F3cQ7YN28',
-                    'https://www.youtube.com/shorts/j_Pv6EHYo9k',
-                    'https://www.youtube.com/shorts/ZTdXLfHWDJE',
-                    'https://www.youtube.com/shorts/x0iC-8ZnNwM']
+    urls_YouTube = [
+        'https://www.youtube.com/shorts/CS3kviWGkH0',
+        'https://www.youtube.com/shorts/-WowH0liGfE',
+        'https://www.youtube.com/shorts/QTyQtQmpZ0g',
+        'https://www.youtube.com/shorts/KDqXneUN4CA',
+        'https://www.youtube.com/shorts/AnOJRqLnfqQ',
+        'https://www.youtube.com/shorts/LcQa94wX-O8',
+        'https://www.youtube.com/shorts/z-F3cQ7YN28',
+        'https://www.youtube.com/shorts/j_Pv6EHYo9k',
+        'https://www.youtube.com/shorts/ZTdXLfHWDJE',
+        'https://www.youtube.com/shorts/x0iC-8ZnNwM'
+    ]
     
-    # Filename to store the URLs
     youtube_videos = 'video_urls.txt'
-    
-    # Save the URLs in a text file called video_urls.txt , where each URL should be stored on a separate line.
     with open(youtube_videos, 'w', encoding='utf-8') as file:
         for text in urls_YouTube:
             file.write(text + '\n')
 
-    # Extract URLs from the text file and store the video names.
     video_urls = extract_urls(youtube_videos)
-
-    # Print the extracted video names
     print(videoenames)
 
-    # Download videos serially
     serial_downloader(video_urls)
-
-    # Download videos in parallel using multiprocessing
     parallel_downloader(video_urls)
-
-    # Download videos in parallel using threading
     parallel_downloader1(video_urls)
-
-    # Download videos in parallel with a limited number of threads using semaphore
     parallel_downloader2(video_urls)
+
+    # Transcribe audio to text after all downloads and audio extractions
+    for audio_file in Path(audio_directory).glob('*.wav'):
+        audio_to_text(audio_file, text_directory)
+
+
+
+
+
+
